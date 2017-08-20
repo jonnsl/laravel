a9dfc87bfd7c705b55fbd2f0aee599670b63c592
Jonnathan Soares <jonnsl@hotmail.com>
2017-08-20 20:22:34 -0300

    Fix password reset token leakage issue.

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -36,4 +36,45 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * This method has two main diferences from the default:
+     * 1st - The token is validated before showing the reset password form.
+     * 2th - The token is removed from the url, for safety reasons see:
+     * https://robots.thoughtbot.com/is-your-site-leaking-password-reset-links
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request)
+    {
+        if ($request->has('token')) {
+            $token = $request->input('token');
+            $user = User::find($request->input('uid'));
+
+            if ($user === null || !$this->broker()->tokenExists($user, $token)) {
+                abort(400);
+            }
+
+            return redirect()
+                ->refresh()
+                ->with([
+                    'token' => $token,
+                    'uid' => $user->id,
+                ]);
+        }
+
+        $session = $request->session();
+        if ($session->has('token') && $session->has('uid')) {
+            return view('auth.passwords.reset')
+                ->with([
+                    'token' => $session->get('token'),
+                    'uid' => $session->get('uid'),
+                ]);
+        }
+
+        abort(400);
+    }
 }
